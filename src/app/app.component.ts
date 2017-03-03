67521c6af58776da09a7b9372dfc940081a6cae7
saibot777 <stefantrajkovic777@gmail.com>
2017-03-03 15:06:04 +0100

    looping through data, and displaying...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  spaceScreens: Array<any>;
+
+  constructor(private http: Http) {
+    this.http.get('./data.json')
+      .map(response => response.json().screenshots)
+      .subscribe(res => this.spaceScreens = res);
+
+  }
 }
